8d5284716655d4a86aca7e2e95032b0d7d9bb0d3
Donjae Marsh <donjaemarsh@gmail.com>
2024-10-26 09:04:29 -0500

    Added click event for squares to alternate between X and O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Select all div elements inside the game board with class `square`
+    
     const squares = document.querySelectorAll("#board div");
 
-    // Loop over each square and set the class attribute to `square`
+    
     squares.forEach(square => {
         square.setAttribute("class", "square");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const squares = document.querySelectorAll("#board .square");
+    let Player = "X";
+    const game_status = Array(9).fill(null); 
+
+    squares.forEach((square, index) => {
+        square.addEventListener("click", () => {
+            
+            if (!game_status[index]) {
+                
+                square.textContent = Player;
+                
+               
+                square.classList.add(Player);
+
+                
+                game_status[index] = Player;
+
+                
+                Player = Player === "X" ? "O" : "X";
+            }
+        });
     });
 });
